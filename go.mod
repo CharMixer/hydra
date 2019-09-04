ffafa0f7918e798edec97039b320280abb3adfc5
Marc <wraix@users.noreply.github.com>
2019-09-04 22:17:57 +0200

    trying to make modules work

--- go.mod
+++ go.mod
@@ -1,6 +1,11 @@
-module github.com/CharMixer/hydra
+module github.com/charmixer/hydra
+
+go 1.12
 
 require (
+	github.com/coreos/go-oidc v2.1.0+incompatible
+	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
 	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	gopkg.in/square/go-jose.v2 v2.3.1 // indirect
 )
---
+++ hydra.go
@@ -0,0 +1,31 @@
+package hydra
+
+import (
+  "log"
+  "os"
+  "net/url"
+  "golang.org/x/net/context"
+  "golang.org/x/oauth2/clientcredentials"
+  oidc "github.com/coreos/go-oidc"
+  "github.com/charmixer/hydra/client"
+)
+
+func main() {
+
+  provider, err := oidc.NewProvider(context.Background(), os.Getenv("HYDRA_URL"))
+  if err != nil {
+    log.Panic(err)
+    return
+  }
+
+  hydraConfig := &clientcredentials.Config{
+    ClientID:     os.Getenv("CLIENT_ID"),
+    ClientSecret: os.Getenv("CLIENT_SECRET"),
+    TokenURL:     provider.Endpoint().TokenURL,
+    Scopes:       []string{"openid"},
+    EndpointParams: url.Values{"audience": {"hydra"}},
+    AuthStyle: 2, // https://godoc.org/golang.org/x/oauth2#AuthStyle
+  }
+
+  _ = client.NewHydraClient(hydraConfig)
+}
